251d966af675f2e8ad28656c587d6e21934eb1a2
nit-designs <nithyae27@gmail.com>
2023-03-14 18:38:57 +0100

    commit first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import { Badge, ButtonToolbar, Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+ 
+  const data = [
+    {
+      productImage:
+        "#",
+      productName: "Fancy Product",
+      price: "$40.00 - $80.00",
+      
+    },
+    {
+      productImage:
+        "#",
+      productName: "Special Item",
+      price:  "$18.00",
+      rating: "⭐⭐⭐⭐⭐",
+    },
+    {
+      productImage:
+        "#",
+      productName: "Sale Item",
+      price: "$25.00",
+      
+    },
+    {
+      productImage:
+        "#",
+      productName: "Popular Item",
+      price: "$40.00",
+      rating: "⭐⭐⭐⭐⭐",
+    },
+    {
+      productImage:
+        "#",
+      productName: "Sale Item",
+      price: "$50.00 $25.00",
+      
+    },
+    {
+      productImage:
+        "#",
+      productName: "Fancy Product",
+      price: "$120.00 - $280.00",
+      
+    },
+    {
+      productImage:
+        "#",
+      productName: "Special Item",
+      price: "$20.00 $18.00",
+      rating: "⭐⭐⭐⭐⭐",
+      
+    },
+    {
+      productImage:
+        "#",
+      productName: "Popular Item",
+      price: "$40.00",
+      rating: "⭐⭐⭐⭐⭐",
+    },
+  ];
+
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cart-value">
+      <Button  variant="light">
+        🛒CART {count}
+      </Button>
+      </div>
+    <div>
+    <h1>Shop in Style</h1>   
+    </div>   
+
+     <div className="card-container">
+      {data.map((prod, idx)=> <Cards
+      idx = {idx}
+      prod = {prod}
+      setCount= {setCount}
+      count = {count}
+      />)}
+
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function Cards({prod, idx, setCount, count}){
+  const [show, setShow] = useState(false);
+
+  function viewOpt(){
+    setShow(!show)
+  }
+
+  function addToCart(){
+    setShow(!show)
+    setCount(count + 1)
+  }
+
+  function removeFromCart(){
+    setShow(!show)
+    setCount(count - 1)
+  }
+
+  return (
+    
+    <Card key={idx} className="card" style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={prod.productImage} style={{ height: "10rem", width: '23rem', backgroundColor: " rgb(216, 216, 220)" }} />
+      <Card.Body>
+        <Card.Title>{prod.productName}</Card.Title>
+        <Card.Text>{prod.price} RS</Card.Text>
+        <Card.Text>{prod.rating} </Card.Text>
+        {/* conditional rendering */}
+        {show ? <Button
+          variant="outline-dark"
+          onClick={viewOpt}
+        >View Options</Button> : ""}
+        {" "}
+
+
+        {!show ? <Button
+          variant="outline-dark"
+          onClick={addToCart}
+        >Add cart</Button> : ""}
+
+
+        {show ? <Button
+          variant="danger"
+          onClick={removeFromCart}
+        >remove cart</Button> : ""}
+
+      </Card.Body>
+    </Card>
+  )
+}
